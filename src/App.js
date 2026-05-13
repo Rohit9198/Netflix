245a3624546bd39a4736c893c14be63e2de38b3e
ROHIT SINGH <rohitsi2252@gmail.com>
2026-05-13 12:21:30 +0530

    3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 
 import './App.css';
+import Body from './components/Body';
 
 function App() {
   return (
-    <div className="text-3xl font-bold text-green-800">
-      Hello Everyone, I am Rohit Singh
-      I am  making Netflix-GPT
-      
-    </div>
+    <Body/>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className="absolute px-8 py-2 bg-gradient-to-b from-black z-10">
+      <img 
+      className="w-44"
+      src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/Netflix-Logo.png/1920px-Netflix-Logo.png?utm_source=commons.wikimedia.org&utm_campaign=index&utm_content=thumbnail&_=20230726112108"
+      alt="logo"/>
+    </div>
+  )
+}
+
+export default Header
