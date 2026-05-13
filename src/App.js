9a616a4234d1222f9307c85d8bdf889569715570
ROHIT SINGH <rohitsi2252@gmail.com>
2026-05-13 10:21:34 +0530

    2

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-green-800">
+      Hello Everyone, I am Rohit Singh
+      I am  making Netflix-GPT
+      
+    </div>
+  );
+}
+
+export default App;
